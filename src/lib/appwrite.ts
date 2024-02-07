c9095f1baab61347caf6438de7055af03960e8dd
Fazal-Code <fazal.rehman@techsembly.com>
2024-02-07 14:10:36 +0500

    chl ja

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -10,18 +10,19 @@ export const account = new Account(client);
 const databases = new Databases(client);
 
 
-const promise = databases.createDocument(
-    '65c33c17a1c8d3d51504',
-    '65c33c367e68080a43e2',
-    ID.unique(),
-    { "book_name": "Hamlet", "book_rating": 4.5 }
-);
+// const promise = databases.createDocument(
+//     '65c33c17a1c8d3d51504',
+//     '65c33c367e68080a43e2',
+//     ID.unique(),
+//     { "book_name": "Hamlet", "book_rating": 4.5 }
+// );
+
+const promise = databases.listDocuments('65c33c17a1c8d3d51504', '65c33c367e68080a43e2');
 
 promise.then(function (response) {
-    console.log(response);
+    console.log(response); // Success
 }, function (error) {
-    console.log(error);
+    console.log(error); // Failure
 });
-
 
 export { ID } from 'appwrite';
